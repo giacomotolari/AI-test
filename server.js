ba660d6660f5d2097a340ba0ae97e8d3eb1ee629
giacomotolari <tolarigiacomo@gmail.com>
2024-02-07 10:40:11 +0100

    feature: add llama model

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ import express from "express";
 import logEndPoints from "./utils/logEndpoints.js";
 import fs from "node:fs";
 
+import Replicate from "replicate";
+
+const token = process.env.AI_API_TOKEN;
+
+const replicate = new Replicate({
+  auth: token,
+});
+
+
+
 const port = process.env.PORT;
 const app = express();
 
@@ -16,9 +26,21 @@ app.get("/ai-answer", (req, res) => {
   res.json({ message: "AI answer" });
 });
 
-
-app.post("/send-prompt", (req, res) => {
-  res.json({ message: "User registered successfully!", prompt: req.body });
+app.post("/send-prompt", async (req, res) => {
+  try {
+    const input = {
+      prompt: req.body.prompt,
+    };
+    for await (const event of replicate.stream("meta/llama-2-70b-chat", {
+      input,
+    })) {
+      process.stdout.write(event.toString());
+    }
+    res.json({ message: "AI-prompt send successfully!", prompt: input });
+  }
+  catch {
+    res.json({ message: "AI-prompt failed to send!" }).status(500);
+  }
 });
 
 app.listen(port, () => {
